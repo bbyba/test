abf35e92e0af467648132ae5c19847a301bc1f7c
Burte Bayarsaikhan <burte.bsn@gmail.com>
2024-02-01 00:36:32 +0800

    Add base project

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Burte Bayaraa on 2024.02.01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
